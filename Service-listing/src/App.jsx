6a1c3a6d11290a18e724e04cd8150ff2744cf8bd
shahbaz12243 <shahbazz12243@gmail.com>
2025-02-27 10:38:15 +0530

    done

---
+++ Service-listing/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ServiceCard from './components/ServiceCard.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Web  Services</h1>
+
+      <ServiceCard 
+        title="Web Development" 
+        description="We specialize in building responsive and modern websites that fulfil to your business needs."
+      />
+      
+      <ServiceCard 
+        title="SEO Optimization" 
+        description="Enhance your website's visibility on search engines to reach a broader audience."
+      />
+      
+    </div>
+  );
+};
+
+export default App;
